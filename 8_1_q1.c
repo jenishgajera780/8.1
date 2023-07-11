7c029ab20a0571fc5c59eca075eef74c9185b2aa
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-11 12:51:56 +0530

    Add files via upload

---
+++ 8_1_q1.c
@@ -0,0 +1,28 @@
+// Q1 write a program to find the length of a 1d array. 
+  
+  
+ #include<stdio.h> 
+ #include<conio.h> 
+  
+ int main(){ 
+  
+ int i,n,g=0; 
+ int array[100]; 
+  
+ clrscr(); 
+  
+ printf("ENTER ARRAY SIZE :- "); 
+ scanf("%d",&n); 
+  
+ for(i=0;i<n;i++) 
+ { 
+ printf("n[%d]= ",i); 
+ scanf("%d",&array[i 
+ ]); 
+ g++; 
+ } 
+ printf("length of array is :- %d",g); 
+ getch(); 
+ }
+     
+    
